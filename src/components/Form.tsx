fd0570654137bf9701bfaa5b1b6f0f7c0f9c4d77
Robinson <robinjosellove@gmail.com>
2023-01-25 15:32:16 -0500

    data validate

---
+++ src/components/Form.tsx
@@ -0,0 +1,145 @@
+import { ChangeEvent, FormEvent, useState } from "react";
+import { UserForm } from "../interfaces/userForm";
+import swal from "sweetalert";
+import "../styles/Form.css";
+
+export const Form = () => {
+  const [userData, setUserData] = useState<UserForm>({
+    name: "",
+    lastName: "",
+    ci: "",
+    age: 0,
+    email: "",
+    phone: "",
+  });
+
+  const handleChange = (e: ChangeEvent<HTMLInputElement>) => {
+    setUserData({
+      ...userData,
+      [e.target.name]: e.target.value,
+    });
+  };
+
+  const { ci } = userData;
+
+  const handleSubmit = (e: FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    const ced = ci;
+
+    let [suma, mul, chars] = [0, 1, ced.length];
+    for (let index = 0; index < chars; index += 1) {
+      let num = Number(ced[index]) * mul;
+      suma += num - Number(num > 9) * 9;
+      mul = 1 << index % 2;
+    }
+
+    if (suma % 10 === 0 && suma > 0) {
+      swal("Buen trabajo!", "Usuario registrado!", "success");
+      setUserData({
+        name: "",
+        lastName: "",
+        ci: "",
+        age: 0,
+        email: "",
+        phone: "",
+      });
+    } else {
+      swal("Algo salio mal!", "Ingresa correctamente tu número de cédula!", "warning");
+      return;
+    }
+  };
+
+  return (
+    <div className="container">
+      <div className="screen">
+        <div className="screen__content">
+          <form className="login" onSubmit={handleSubmit}>
+            <div className="login__field">
+              <i className="login__icon fas fa-user"></i>
+              <input
+                type="text"
+                className="login__input"
+                name="name"
+                placeholder="Nombre"
+                required
+                minLength={3}
+                maxLength={60}
+                onChange={handleChange}
+              />
+            </div>
+            <div className="login__field">
+              <i className="login__icon fas fa-lock"></i>
+              <input
+                type="text"
+                className="login__input"
+                name="lastName"
+                placeholder="Apellido"
+                required
+                minLength={3}
+                maxLength={60}
+                onChange={handleChange}
+              />
+            </div>
+            <div className="login__field">
+              <i className="login__icon fas fa-lock"></i>
+              <input
+                type="text"
+                className="login__input"
+                name="ci"
+                required
+                placeholder="Cédula"
+                onChange={handleChange}
+              />
+            </div>
+            <div className="login__field">
+              <i className="login__icon fas fa-lock"></i>
+              <input
+                type="number"
+                className="login__input"
+                name="age"
+                placeholder="Edad"
+                required
+                min={1}
+                max={100}
+                onChange={handleChange}
+              />
+            </div>
+            <div className="login__field">
+              <i className="login__icon fas fa-lock"></i>
+              <input
+                type="email"
+                className="login__input"
+                name="email"
+                placeholder="Email"
+                required
+                onChange={handleChange}
+              />
+            </div>
+            <div className="login__field">
+              <i className="login__icon fas fa-lock"></i>
+              <input
+                type="tel"
+                className="login__input"
+                name="phone"
+                placeholder="Teléfono"
+                required
+                minLength={10}
+                onChange={handleChange}
+              />
+            </div>
+            <button className="button login__submit" type="submit">
+              <span className="button__text">Registrar</span>
+              <i className="button__icon fas fa-chevron-right"></i>
+            </button>
+          </form>
+        </div>
+        <div className="screen__background">
+          <span className="screen__background__shape screen__background__shape4"></span>
+          <span className="screen__background__shape screen__background__shape3"></span>
+          <span className="screen__background__shape screen__background__shape2"></span>
+          <span className="screen__background__shape screen__background__shape1"></span>
+        </div>
+      </div>
+    </div>
+  );
+};
---
+++ src/interfaces/userForm.ts
@@ -0,0 +1,8 @@
+export interface UserForm {
+    name:     string;
+    lastName: string;
+    ci:       string;
+    age:      number;
+    email:    string;
+    phone:    string;
+}
